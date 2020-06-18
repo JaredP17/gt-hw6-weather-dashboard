86c2d358c9bf4de7a33479ccc06a2f913653d1bb
Jared Porter <jaredp17@me.com>
2020-06-18 19:07:22 -0400

    Getting response from api. Time to render weather data dynamically.

--- Assets/script.js
+++ Assets/script.js
@@ -1,14 +1,32 @@
 $(document).ready(function () {
-    // VARIABLES
-    var now = moment().format("(M/D/YYYY)");
+  // VARIABLES
+  var now = moment().format("(M/D/YYYY)");
+  var apiKey = "a33bca960d808238c931cd8829f838b1";
+  
+  // FUNCTION DEFINITIONS
 
-    console.log(now);
-    // FUNCTION DEFINITIONS
+  // FUNCTION CALLS
 
-    // FUNCTION CALLS
+  // EVENT LISTENERS
+  $("#search-form").on("submit", function (event) {
+    event.preventDefault();
+    var cityInput = $("#search-input").val();
+    // console.log(cityInput);
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
 
-    // EVENT LISTENERS
-   
+    // Get query response
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+        // Render dashboard
+      }).catch(function (e) {
+        console.log(e);
+        alert(e.responseJSON.cod + ": " + e.responseJSON.message); 
+      });
+  });
+ 
+
+  
 });
-
-
