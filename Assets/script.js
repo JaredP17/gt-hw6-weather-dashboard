6dd72ec918291d068dde17e5351090bd895ec0a5
Jared Porter <jaredp17@me.com>
2020-06-18 18:00:19 -0400

    Made some style changes. Ready for DOM manipulation with jQuery.

---
+++ Assets/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    // VARIABLES
+    var now = moment().format("(M/D/YYYY)");
+
+    console.log(now);
+    // FUNCTION DEFINITIONS
+
+    // FUNCTION CALLS
+
+    // EVENT LISTENERS
+   
+});
+
+
