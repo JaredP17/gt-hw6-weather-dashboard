7116cdb41f68c9e78c5f6c371e706196bb417dc4
Jared Porter <jaredp17@me.com>
2020-06-18 23:07:13 -0400

    Successfully populated main dashboard.

--- Assets/script.js
+++ Assets/script.js
@@ -13,14 +13,52 @@ $(document).ready(function () {
     var cityInput = $("#search-input").val();
     // console.log(cityInput);
     var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
+    var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`;
+
 
     // Get query response
     $.ajax({
         url: queryURL,
-        method: "GET",
+        method: "GET"
       }).then(function (response) {
         console.log(response);
         // Render dashboard
+        // Set City/Current Date
+        $("#dashboard-title").html(`${response.name} ${now} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
+
+        // Set temperature
+        var tempF =((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
+        $("#temperature").text(tempF + " \u2109");
+
+        // Set humidity
+        var humidity = response.main.humidity;
+        $("#humidity").text(humidity + "%");
+
+        // Set wind speed
+        var windSpeed = response.wind.speed;
+        $("#wind-speed").text(windSpeed + " MPH")
+
+        // Set UV index
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+        $.ajax({
+          url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
+          method: "GET"
+        }).then(function (response) {
+          var uvIndex = response.value;
+          var condition = "btn-danger";
+          if (uvIndex < 3) {
+            condition = "btn-success";
+          } else if (uvIndex < 7) {
+            condition = "btn-warning";
+          }
+          $("#uv-index").text(uvIndex);
+          $("#uv-index").attr("class", `btn ${condition}`);
+        });
+        
+
+
+
       }).catch(function (e) {
         console.log(e);
         alert(e.responseJSON.cod + ": " + e.responseJSON.message); 
