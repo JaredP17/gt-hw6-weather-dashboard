51c4042de70c8fead36c5defde1e64aaa8b49b3e
Jared Porter <jaredp17@me.com>
2020-06-19 03:01:15 -0400

    Forecast renders properly. Working on persistent storage.

--- Assets/script.js
+++ Assets/script.js
@@ -2,69 +2,106 @@ $(document).ready(function () {
   // VARIABLES
   var now = moment().format("(M/D/YYYY)");
   var apiKey = "a33bca960d808238c931cd8829f838b1";
-  
+  var citiesSearched = [];
+  var currentCity;
+
   // FUNCTION DEFINITIONS
+
+  // Loads the main dashboard of current city weather
+  function populateDashboard(response) {
+    // Set City/Current Date
+    $("#dashboard-title").html(
+      `${response.name} ${now} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png" alt="${response.weather[0].description}">`
+    );
+
+    // Set temperature
+    var tempF = response.main.temp.toFixed(1); // Fixed 1 decimal place
+    $("#temperature").text(tempF + " \u2109");
+
+    // Set humidity
+    var humidity = response.main.humidity;
+    $("#humidity").text(humidity + "%");
+
+    // Set wind speed
+    var windSpeed = response.wind.speed;
+    $("#wind-speed").text(windSpeed + " MPH");
+
+    // Coordinates
+    var lon = response.coord.lon;
+    var lat = response.coord.lat;
+
+    // Set UV index
+    setUVIndex(apiKey, lat, lon);
+
+    // Populate 5-Day Forecast
+    populateForecast(apiKey, lat, lon);
+  }
+
+  function setUVIndex(apiKey, lat, lon) {
+    $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var uvIndex = response.value;
+      var condition = "btn-danger"; // Severe
+      if (uvIndex < 3) {
+        condition = "btn-success"; // Preferable
+      } else if (uvIndex < 7) {
+        condition = "btn-warning"; // Moderate
+      }
+      $("#uv-index").text(uvIndex);
+      $("#uv-index").attr("class", `btn ${condition}`);
+    });
+  }
+
+  function populateForecast(apiKey, lat, lon) {
+    $.ajax({
+      url: `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,hourly&appid=${apiKey}&units=imperial`,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+      var dailyForecasts = $("#5-days");
+      dailyForecasts.empty();
+
+      for (var i = 1; i <= 5; i++) {
+        var nextDay = moment().add(i, "days").format("M/D/YYYY");
+        dailyForecasts.append(
+          `<div class="card bg-primary col-md-2 mb-2 pt-2 text-white">
+            <h4>${nextDay}</h4>
+            <p>
+              <img src="http://openweathermap.org/img/wn/${response.daily[i].weather[0].icon}.png" alt="${response.daily[i].weather[0].description}">
+            </p>
+            <p>Temp: ${response.daily[i].temp.day} \u2109</p>
+            <p>Humidity: ${response.daily[i].humidity}%</p>`
+        );
+      }
+    });
+  }
 
   // FUNCTION CALLS
 
   // EVENT LISTENERS
   $("#search-form").on("submit", function (event) {
     event.preventDefault();
-    var cityInput = $("#search-input").val();
+    var cityInput = $("#search-input").val().trim();
     // console.log(cityInput);
-    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}`;
-    var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&appid=${apiKey}`;
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=imperial`;
 
-
-    // Get query response
+    // Get current response
     $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function (response) {
+      url: queryURL,
+      method: "GET",
+    })
+      .then(function (response) {
         console.log(response);
-        // Render dashboard
-        // Set City/Current Date
-        $("#dashboard-title").html(`${response.name} ${now} <img src="http://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png">`);
 
-        // Set temperature
-        var tempF =((response.main.temp - 273.15) * 1.8 + 32).toFixed(1);
-        $("#temperature").text(tempF + " \u2109");
-
-        // Set humidity
-        var humidity = response.main.humidity;
-        $("#humidity").text(humidity + "%");
-
-        // Set wind speed
-        var windSpeed = response.wind.speed;
-        $("#wind-speed").text(windSpeed + " MPH")
-
-        // Set UV index
-        var lon = response.coord.lon;
-        var lat = response.coord.lat;
-        $.ajax({
-          url: `https://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`,
-          method: "GET"
-        }).then(function (response) {
-          var uvIndex = response.value;
-          var condition = "btn-danger";
-          if (uvIndex < 3) {
-            condition = "btn-success";
-          } else if (uvIndex < 7) {
-            condition = "btn-warning";
-          }
-          $("#uv-index").text(uvIndex);
-          $("#uv-index").attr("class", `btn ${condition}`);
-        });
-        
-
-
-
-      }).catch(function (e) {
+        // Populate dashboard
+        populateDashboard(response);
+      })
+      .catch(function (e) {
         console.log(e);
-        alert(e.responseJSON.cod + ": " + e.responseJSON.message); 
+        alert(e.responseJSON.cod + ": " + e.responseJSON.message);
       });
   });
- 
-
-  
 });
